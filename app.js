cbbf11fcd5e7fe72294da777bdc9c5a0834e7a07
faiza latif <faiza@outlook.pt>
2021-09-27 14:56:18 +0100

    Javascript to Vue3

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        }
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue)
+            this.enteredValue = '';
+        }
+    }
+}).mount('#app');
+/*
 const buttonEl = document.querySelector('button');
 const inputEl = document.querySelector('input');
 const listEl = document.querySelector('ul');
@@ -9,4 +24,5 @@ buttonEl.addEventListener('click', () => {
     listEl.append(listItemEl);
     inputEl.value = '';
 })
+*/
 
